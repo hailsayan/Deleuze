bb100e19b9953e5bed288f7241fb128f7917bd98
Psyon <sayan.h2020@gmail.com>
2024-11-26 16:55:47 +0330

    feat: add command cases

--- main.go
+++ main.go
@@ -22,34 +22,83 @@ type element struct {
 	next *element
 }
 
+func (e *element) getFirstElement() (current *element) {
+	current = e
+
+	for {
+		if current.prev == nil {
+			break
+		}
+		current = current.prev
+	}
+
+	return
+}
+
+func (e *element) print() {
+	current := e.getFirstElement()
+	var sb strings.Builder
+
+	for {
+		sb.WriteByte(current.data)
+		if current.next == nil {
+			break
+		} else {
+			current = current.next
+		}
+	}
+
+	fmt.Println(sb.String())
+}
+
 func main() {
+	curser := &element{data: '|'}
+
 	var count int
 	fmt.Scan(&count)
 
 	reader := bufio.NewReader(os.Stdin)
 
-	for idx := 0; idx < count; idx++ {
+	for index := 0; index < count; index++ {
 		line, _ := reader.ReadString('\n')
 		line = strings.TrimSuffix(line, "\n")
 		fields := strings.Fields(line)
 
 		switch command(fields[0]) {
 		case INSERT:
-			fmt.Println("INSERT")
+			element := &element{data: []byte(fields[1])[0]}
+			if curser.prev != nil {
+				element.prev = curser.prev
+				curser.prev.next = element
+			}
+			curser.prev = element
+			element.next = curser
 
 		case LEFT:
-			fmt.Println("LEFT")
+			if curser.prev != nil {
+				curser.prev.data, curser.data = curser.data, curser.prev.data
+				curser = curser.prev
+			}
 
 		case RIGHT:
-			fmt.Println("RIGHT")
+			if curser.next != nil {
+				curser.next.data, curser.data = curser.data, curser.next.data
+				curser = curser.next
+			}
 
 		case BACKSPACE:
-			fmt.Println("BACKSPACE")
-
+			if curser.prev != nil {
+				if curser.prev.prev != nil {
+					curser.prev.prev.next = curser
+					curser.prev = curser.prev.prev
+				} else {
+					curser.prev = nil
+				}
+			}
 		default:
-			panic("invalid command given")
-
+			panic("invalid command")
 		}
-
 	}
+
+	curser.print()
 }
