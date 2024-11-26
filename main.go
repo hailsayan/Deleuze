77ec0d8b211f66b07a85cefc415d9e16430f964b
Psyon <sayan.h2020@gmail.com>
2024-11-26 13:27:18 +0330

    feat: add base of cli

--- main.go
+++ main.go
@@ -1,5 +1,55 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+type command string
+
+const (
+	INSERT    command = "INSERT"
+	LEFT      command = "LEFT"
+	RIGHT     command = "RIGHT"
+	BACKSPACE command = "BACKSPACE"
+)
+
+type element struct {
+	data byte
+	prev *element
+	next *element
+}
+
+func main() {
+	var count int
+	fmt.Scan(&count)
+
+	reader := bufio.NewReader(os.Stdin)
+
+	for idx := 0; idx < count; idx++ {
+		line, _ := reader.ReadString('\n')
+		line = strings.TrimSuffix(line, "\n")
+		fields := strings.Fields(line)
+
+		switch command(fields[0]) {
+		case INSERT:
+			fmt.Println("INSERT")
+
+		case LEFT:
+			fmt.Println("LEFT")
+
+		case RIGHT:
+			fmt.Println("RIGHT")
+
+		case BACKSPACE:
+			fmt.Println("BACKSPACE")
+
+		default:
+			panic("invalid command given")
+
+		}
+
+	}
 }
